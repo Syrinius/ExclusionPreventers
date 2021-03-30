ac7a62397b6ad4bc370c90d060bdc4b3df54d948
Tuukka Tuominen <tao.tuominen@gmail.com>
2021-03-30 19:33:58 +0300

    modified NextButtonPosition class

--- core/src/fi/tuni/tiko/utilities/NextButtonPosition.java
+++ core/src/fi/tuni/tiko/utilities/NextButtonPosition.java
@@ -8,10 +8,11 @@ import fi.tuni.tiko.sceneSystem.MainMenu;
 
 public class NextButtonPosition {
 
-
+    //use for menu elements
     public static MenuPosition nextMenuPosition(MenuPosition position){
         return new MenuPosition(200,100);
     }
+    //use for level elements
     public static MenuPosition nextLevelPosition(MenuPosition position){
         return new MenuPosition(150, 150);
     }
